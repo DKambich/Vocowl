224b38ac693235fa42d6544aac1924caced3caf8
Daniel Kambich <daniel.kambich@gmail.com>
2022-11-08 15:56:35 -0600

    Add baseline app skeleton

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,39 @@
 <script lang="ts">
-  import { SvelteUIProvider, Text } from "@svelteuidev/core";
+  import {
+    AppShell,
+    Aside,
+    Button,
+    Footer,
+    Header,
+    Navbar,
+    ShellSection,
+    SvelteUIProvider,
+    Text,
+  } from "@svelteuidev/core";
+  let opened = false;
 </script>
 
 <SvelteUIProvider withNormalizeCSS withGlobalStyles>
-  <Text size="xl">Hello World</Text>
+  <AppShell fixed navbarOffsetBreakpoint="xl">
+    <Navbar
+      slot="navbar"
+      hiddenBreakpoint="xl"
+      hidden={!opened}
+      fixed
+      width={{
+        sm: 300,
+        md: 350,
+        lg: 400,
+        xl: 450,
+        base: 100,
+      }}
+    >
+      Navbar Content
+    </Navbar>
+
+    <Header height={60} fixed slot="header">Header Content</Header>
+    <!-- Main content uses the default slot, so no need to explicitly declare it -->
+    <ShellSection grow>Main Content</ShellSection>
+    <Footer height={60} slot="footer">Footer Content</Footer>
+  </AppShell>
 </SvelteUIProvider>
