257f587866ee6ccdcfcc0b6565b2d4687c9e11ce
Daniel Kambich <daniel.kambich@gmail.com>
2022-11-09 16:28:05 -0600

    Cleanup and coment Header and themStore

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,55 +1,50 @@
 <script lang="ts">
+  // Module Imports
   import {
     Navbar,
     NavBrand,
     NavHamburger,
     NavLi,
     NavUl,
+    Tooltip,
   } from "flowbite-svelte";
-  import { theme, toggleTheme } from "../stores/themeStore";
-  import Vocowl from "./Vocowl.svelte";
   import { Moon, Sun } from "svelte-heros-v2";
 
-  let isDarkMode;
+  // Local Imports
+  import { theme, toggleTheme } from "../stores/themeStore";
+  import Vocowl from "./Vocowl.svelte";
 
+  // Listen for changes in theme to update styles accordingly
+  let isDarkMode;
   theme.subscribe((value) => {
     isDarkMode = value === "dark";
   });
 </script>
 
 <Navbar let:hidden let:toggle color={isDarkMode ? "dark" : "indigo"}>
-  <NavBrand href="/">
-    <div class="mr-3 h-6 sm:h-9 flex items-center dark:text-white">
-      <Vocowl size={24} />
+  <!-- Vocowl Logo and Title -->
+  <NavBrand href="./">
+    <div class="mx-3 dark:text-white">
+      <Vocowl size={28} />
     </div>
-    <span
-      class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
-    >
-      Vocowl
-    </span>
+    <span class="text-xl font-semibold dark:text-white">Vocowl</span>
   </NavBrand>
+  <!-- Hamburger Menu Icon displayed on small screens -->
   <NavHamburger on:click={toggle} />
+  <!-- List of Navigation Links -->
   <NavUl {hidden}>
     <NavLi href="./">Home</NavLi>
     <NavLi href="./about">About</NavLi>
     <NavLi href="./select">Select</NavLi>
+    <!-- Theme Toggle Button -->
     <NavLi on:click={toggleTheme}>
-      <div class="cursor-pointer">
-        <span class="sm:hidden md:inline">
-          {#if isDarkMode}
-            <Moon size="20" />
-          {:else}
-            <Sun size="20" />
-          {/if}
-        </span>
-        <span class="flex items-center gap-x-2 md:hidden ">
-          Toggle Theme
-          {#if isDarkMode}
-            <Moon size="21" />
-          {:else}
-            <Sun size="21" />
-          {/if}
-        </span>
+      <div class="cursor-pointer flex items-center gap-x-2">
+        <span class="md:hidden">Toggle Theme</span>
+        {#if isDarkMode}
+          <Moon size="21" />
+        {:else}
+          <Sun size="21" />
+        {/if}
       </div>
     </NavLi>
   </NavUl>
--- src/stores/themeStore.ts
+++ src/stores/themeStore.ts
@@ -3,18 +3,23 @@ import { writable } from "svelte/store";
 export const theme = writable("light");
 
 export const toggleTheme = () => {
-  theme.update((theme) => (theme === "light" ? "dark" : "light"));
+  // Update Theme Store
+  theme.update((storeTheme) => (storeTheme === "light" ? "dark" : "light"));
 
-  if (localStorage.getItem("color-theme")) {
-    if (localStorage.getItem("color-theme") === "light") {
+  //  Check if local storage has a theme preference saved
+  const localTheme = localStorage.getItem("color-theme");
+  if (localTheme) {
+    // Set app theme according to saved local theme
+    if (localTheme === "light") {
       document.documentElement.classList.add("dark");
-      localStorage.setItem("color-theme", "dark");
     } else {
       document.documentElement.classList.remove("dark");
-      localStorage.setItem("color-theme", "light");
     }
-    // if NOT set via local storage previously
+    // Save the new theme
+    const toggledTheme = localTheme === "light" ? "dark" : "light";
+    localStorage.setItem("color-theme", toggledTheme);
   } else {
+    // if theme is not in local storage previously, update theme based on class list
     if (document.documentElement.classList.contains("dark")) {
       document.documentElement.classList.remove("dark");
       localStorage.setItem("color-theme", "light");
@@ -25,18 +30,17 @@ export const toggleTheme = () => {
   }
 };
 
-export const setTheme = (darkMode: boolean) => {};
-
+// Initial theme logic when app loads
 if (
   localStorage.getItem("color-theme") === "dark" ||
   (!("color-theme" in localStorage) &&
     window.matchMedia("(prefers-color-scheme: dark)").matches)
 ) {
+  // Use a dark theme if stored by local storage or if not stored and user prefers dark theme
   document.documentElement.classList.add("dark");
   theme.set("dark");
 } else {
+  // Otherwise use a light theme
   document.documentElement.classList.remove("dark");
   theme.set("light");
 }
-
-console.log(theme);
