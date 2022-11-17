fb34e51106139401cbfb11d12738e1430be0cceb
Daniel Kambich <daniel.kambich@gmail.com>
2022-11-16 20:57:27 -0600

    Add search radius control to nearby search

--- src/components/NearbySearch.svelte
+++ src/components/NearbySearch.svelte
@@ -1,27 +1,56 @@
 <script lang="ts">
-  import { Button, Listgroup, ListgroupItem, P } from "flowbite-svelte";
+  import { Label, Listgroup, ListgroupItem, P, Range } from "flowbite-svelte";
   import { afterUpdate } from "svelte";
-  import { Plus, PlusCircle } from "svelte-heros-v2";
-  import { cacheLocation, preferences } from "../stores/preferencesStore";
+  import { PlusCircle } from "svelte-heros-v2";
+  import { MILES_TO_METERS } from "../constants";
 
   let buttons = Array(20)
     .fill(0)
     .map((x, i) => ({ name: i }));
 
+  // External user location
   export let location: google.maps.LatLngLiteral;
+  // Internal state variables
   let nearbyMap: google.maps.Map;
+  let searchCircle: google.maps.Circle;
+  let searchRadius = 5;
 
+  // Update map center and search radius center when location changes
+  $: {
+    // TODO: Run the nearby places search
+    nearbyMap?.moveCamera({ center: location });
+    searchCircle?.setCenter(location);
+  }
+
+  // Update map center and search radius center when location changes
+  $: {
+    // TODO: Run the nearby places search
+    searchCircle?.setRadius(searchRadius * MILES_TO_METERS);
+  }
+
+  // After DOM has loaded, load in the the nearby search map and search radius
   afterUpdate(() => {
     if (location && !nearbyMap) {
       nearbyMap = new google.maps.Map(document.getElementById("nearbyMap"), {
         center: location,
-        zoom: 12,
+        zoom: 11,
       });
-    } else if (location && nearbyMap) {
-      // Move the map camera if location has shifted
-      nearbyMap.moveCamera({ center: location });
+      searchCircle = new google.maps.Circle({
+        map: nearbyMap,
+        center: location,
+        radius: searchRadius * MILES_TO_METERS,
+        strokeWeight: 0,
+        fillColor: "#5850EC",
+        fillOpacity: 0.35,
+      });
     }
   });
+
+  function updateRadius() {
+    // TODO: Set the new locations
+  }
+
+  console.log();
 </script>
 
 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
@@ -34,29 +63,40 @@
       location.</P
     >
   </div>
-  <Listgroup class="h-fit max-h-[500px] overflow-auto">
-    <h3
-      class="text-center bg-primary-700 py-2 text-white font-bold rounded-t-lg sticky top-0"
-    >
-      Nearby Results
-    </h3>
-    {#each buttons as button}
-      <div class="px-2">
-        <ListgroupItem class="flex items-center">
-          <div class="grow wrap pr-2">
-            <div class="font-bold break-all">
-              {button.name.toString().repeat(35)}
+  <div class="flex flex-col max-h-[500px] gap-y-4">
+    <div>
+      <Label>Search Radius: {searchRadius} miles</Label>
+      <Range
+        min="1"
+        max="10"
+        bind:value={searchRadius}
+        on:change={updateRadius}
+      />
+    </div>
+    <Listgroup class="h-fit overflow-auto grow">
+      <h3
+        class="text-center bg-primary-700 py-2 text-white font-bold rounded-t-lg sticky top-0"
+      >
+        Nearby Results
+      </h3>
+      {#each buttons as button}
+        <div class="px-2">
+          <ListgroupItem class="flex items-center">
+            <div class="grow wrap pr-2">
+              <div class="font-bold break-all">
+                {button.name.toString().repeat(35)}
+              </div>
+              <div class="font-semibold underline  break-all">
+                123 Apple Street
+              </div>
             </div>
-            <div class="font-semibold underline  break-all">
-              123 Apple Street
-            </div>
-          </div>
-          <PlusCircle class="min-w-max" />
-        </ListgroupItem>
-      </div>
-    {/each}
-    {#if buttons.length === 0}
-      <div class="text-center py-4">No Results Found...</div>
-    {/if}
-  </Listgroup>
+            <PlusCircle class="min-w-max" />
+          </ListgroupItem>
+        </div>
+      {/each}
+      {#if buttons.length === 0}
+        <div class="text-center py-4">No Results Found...</div>
+      {/if}
+    </Listgroup>
+  </div>
 </div>
--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,6 @@
 export const HOME_ROUTE: string = "/",
   ABOUT_ROUTE: string = "/about/",
   SEARCH_ROUTE: string = "/search/";
+
+export const MILES_TO_METERS = 1609.344;
+export const METERS_TO_MILES = 0.0006213712;
