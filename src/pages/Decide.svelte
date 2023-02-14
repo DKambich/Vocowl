807cdda0b2e6e0736d8ccdd9ed9b65ce0bf81ba8
Daniel Kambich <daniel.kambich@gmail.com>
2023-02-13 20:38:52 -0600

    Begin work on Decide Page

--- src/pages/Decide.svelte
+++ src/pages/Decide.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
+  import { Button } from "flowbite-svelte";
   import { PageBaseline } from ".";
+  import { preferences } from "../stores/preferencesStore";
+  let restaurants = $preferences.restaurants;
 </script>
 
 <PageBaseline>
-  <div class="dark:text-white">Decide Page</div>
+  <div class="flex flex-col justify-center items-center">
+    <div class="dark:text-white">Decide Page</div>
+    {#each restaurants as restaurant}
+      {restaurant.name}<br />
+    {/each}
+    <Button color="primary" size="xl" class="w-full md:w-auto">Spin</Button>
+  </div>
 </PageBaseline>
